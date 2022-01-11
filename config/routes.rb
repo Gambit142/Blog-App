0c269a64803aa0b7cb8d73527232e0a3ac6f4135
Gambit142 <nzefrancismaryeugorji@gmail.com>
2022-01-12 00:06:26 +0100

    Create routes for users and posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/index'
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show]
+  end
+  root 'users#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
