0519d41340c07f81b9e7d39222dbce2a4d1612a4
Gambit142 <nzefrancismaryeugorji@gmail.com>
2022-01-12 18:08:58 +0100

    Write requests spec tests for Users Controller

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,6 +2,20 @@ require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+    it "tests the URL path for users#index" do
+      get users_path
+
+      expect(response.status).to have_http_status(200)
+      expect(response).to render_template(:index)
+      expect(response.body).to include("<h1>Here is a list of users in the database</h1>")
+    end
+
+    it "tests the URL path for users#show" do
+      get user_path
+
+      expect(response.status).to have_http_status(200)
+      expect(response).to render_template(:show)
+      expect(response.body).to include("<h1>Here is the view for only one user</h1>")
+    end
   end
 end
