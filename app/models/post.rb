e51fe32c6ad64673b53cac21e22405476fb132d8
Gambit142 <nzefrancismaryeugorji@gmail.com>
2022-01-18 10:05:50 +0100

    Add methods to the Post Class that updates User's post comments and returns the first five most recent comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,14 @@
 class Post < ApplicationRecord
   belongs_to: user
   has_many :comments
+  after_save :update_post_counter
+
+  def five_recent_comments
+    comments.order(created_at: :DESC).limit(5)
+  end
+
+  private
+  def update_post_counter
+    user.increment!(:posts_counter)
+  end
 end
