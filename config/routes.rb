55702a9b204e9fc6f2392cf7b68f2bfabd13073d
Gambit142 <nzefrancismaryeugorji@gmail.com>
2022-01-20 15:29:12 +0100

    Add extra routes to existing posts routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show]
+    resources :posts, only: [:index, :show, :new, :create]
   end
   root 'users#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
