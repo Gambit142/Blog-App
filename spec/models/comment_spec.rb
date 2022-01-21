48e9f53e8c3b89958a5d6655171eb0070fa99da9
Gambit142 <nzefrancismaryeugorji@gmail.com>
2022-01-21 23:04:50 +0100

    Write unit tests to test the methods of the Comment model

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { 
+    user1 = User.new(name: 'Gambit', photo: 'string', bio: 'string', posts_counter: 0)
+    Post.create!(title: 'Business', text: 'This is a business post', likes_counter: 0, comments_counter: 0, user: user1)
+  }
+  
+  describe 'test comments methods' do
+    it 'should test the functionality of the update_comments_counter' do
+      user2 = User.create!(name: 'Thunder Cat', photo: 'photo4', bio: 'Lorem ipsum dolor', posts_counter: 0)
+      comment1 = user2.comments.create!(text: 'first_comment', post: subject)
+      comment2 = user2.comments.create!(text: 'second_comment', post: subject)
+      comment3 = user2.comments.create!(text: 'third_comment', post: subject)
+
+      expect(subject.comments_counter).to eql 3
+    end
+  end
 end
