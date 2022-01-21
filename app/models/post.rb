e116a71110b08c509f6ad50204da4f0d71702856
Gambit142 <nzefrancismaryeugorji@gmail.com>
2022-01-21 22:59:46 +0100

    Validate the attributes in the User model

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,10 @@ class Post < ApplicationRecord
   has_many :comments
   after_save :update_posts_counter
 
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :comments_counter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
   def five_recent_comments
     comments.order(created_at: :DESC).limit(5)
   end
