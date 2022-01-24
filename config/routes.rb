eff69ee111ba791d0154a08369372da5ca8019a8
Gambit142 <nzefrancismaryeugorji@gmail.com>
2022-01-24 23:54:48 +0100

    Change the home page of the application

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :new, :create]
   end
-  root 'users#index'
+  root 'homes#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
