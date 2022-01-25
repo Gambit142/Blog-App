e2003e51c310b21cc0224df3edf155f8671fd64a
Gambit142 <nzefrancismaryeugorji@gmail.com>
2022-01-26 00:11:42 +0100

    Install cancancan and initialize its ability

---
+++ app/models/ability.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    user ||= User.new # guest user (not logged in)
+    if user.admin?
+      can :manage, :all
+    else
+      can :manage, Post, user_id: user.id
+      can :manage, Comment, user_id: user.id
+      can :read, :all
+    end
+  end
+end
