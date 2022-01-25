f39ce0df38b0f4cb56fb88368b1524297bef93ea
Gambit142 <nzefrancismaryeugorji@gmail.com>
2022-01-25 22:36:00 +0100

    Fix bugs in project

--- db/schema.rb
+++ db/schema.rb
@@ -49,7 +49,7 @@ ActiveRecord::Schema.define(version: 2022_01_24_203126) do
     t.string "name"
     t.string "photo"
     t.text "bio"
-    t.integer "posts_counter"
+    t.integer "posts_counter", default: 0
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "email", default: "", null: false
