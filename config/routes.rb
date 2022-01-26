4ef699491a6803adaba50ddc15b8af6f9402f311
mmsesay <maej_01@hotmail.com>
2022-01-26 00:39:23 +0000

    Add: functionality to delete a comment

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   post 'users/:user_id/posts/:id/create_comment', to: 'comments#create', as: 'create_comment'
   post 'users/:user_id/posts/:id/create_like', to: 'likes#create', as: 'like_comment'
+  delete 'users/:user_id/posts/:post_id/delete_comment/:id', to: 'comments#delete', as: 'delete_comment' 
 
   resources :users, only: [:index, :show] do
     resources :posts
