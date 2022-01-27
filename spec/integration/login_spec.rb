d8304d98d69ec89697970de3d4e4824876812e9e
mmsesay <maej_01@hotmail.com>
2022-01-27 00:49:05 +0000

    Add: tests for login page

--- spec/integration/login_spec.rb
+++ spec/integration/login_spec.rb
@@ -4,15 +4,41 @@ RSpec.describe 'Login Features', type: :system do
   # before :each do
   #   User.make(email: 'user@example.com', password: 'password')
   # end
-  it "signs me in" do
+
+  it 'login page contains email, password and button elements' do
     visit new_user_session_path
-    # within("#session") do
-    #   fill_in 'Email', with: 'user@example.com'
-    #   fill_in 'Password', with: 'password'
-    # end
-    # click_button 'Sign in'
-    expect(page).to have_selector 'test-email'
-    expect(page).to have_selector 'test-password'
-    expect(page).to have_selector 'test- button'
+    expect(page).to have_css '.test-email'
+    expect(page).to have_css '.test-password'
+    expect(page).to have_css '.test-button'
+  end
+
+  it 'return error if email and password fields are empty' do
+    visit new_user_session_path
+    within('.form-container') do
+      fill_in 'Email', with: ''
+      fill_in 'Password', with: ''
+    end
+    click_button 'Log in'
+    expect(page).to have_text 'Invalid Email or password.'
+  end
+
+  it 'return error if email and password fields are incorrect' do
+    visit new_user_session_path
+    within('.form-container') do
+      fill_in 'Email', with: 'bob@test.com'
+      fill_in 'Password', with: 'bob1234'
+    end
+    click_button 'Log in'
+    expect(page).to have_text 'Invalid Email or password.'
+  end
+
+  it 'redirect to the homepage after login' do
+    visit new_user_session_path
+    within('.form-container') do
+      fill_in 'Email', with: 'tom@test.com'
+      fill_in 'Password', with: '123456'
+    end
+    click_button 'Log in'
+    expect(page).to have_current_path(root_path)
   end
 end
