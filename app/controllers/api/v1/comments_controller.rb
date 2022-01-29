d11dab2a97cdd0ce18b5de53bbb99d93b9b61386
mmsesay <maej_01@hotmail.com>
2022-01-29 06:27:17 +0000

    retouch the sveing of a comment

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -2,37 +2,27 @@ module Api
   module V1
     class CommentsController < Api::V1::ApplicationController
       before_action :authenticate_user!
-      # ALLOWED_DATA = %[text].freeze
 
       def index
         @comments = find_post.comments.includes(:user)
       end
 
       def create
-        @comment = Comment.new(comments_params)
-        @comment.post_id = params[:post_id]
-        @comment.user_id = current_user.id
+        comment = Comment.new(comments_params)
+        comment.post_id = params[:post_id]
+        comment.user_id = current_user.id
 
-        if @comment.save
-          render json: { status: 201, message: 'Comment created successfully!', content: { comment: @comment } }
+        if comment.save
+          render json: { status: 201, message: 'Comment created successfully!', content: { comment: comment } }
         else
-          render json: @comment.errors, status: :bad_request, message: 'Operation failed'
+          render json: comment.errors, status: :bad_request, message: 'Operation failed'
         end
       end
 
       private
 
-      # def find_user
-      #   User.find(params[:user_id])
-      # end
-
-      # def find_post
-      #   find_user.posts.find(params[:post_id])
-      #   # post
-      # end
-
       def comments_params
-        params.permit(:text)
+        params.require(:comment).permit(:text)
       end
     end
   end
